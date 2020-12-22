f0459f3eb4ed95d8880cf5449dfb74cd25579aac
maloneykevin <kevin.maloney@emory.edu>
2020-12-22 16:50:05 -0500

    Stratified tables by ptype
    Renamed files for organization. Created new tables with mixing patterns stratified by partnership type.

---
+++ 4 Analysis - By ptype.R
@@ -0,0 +1,102 @@
+rm(list = ls())
+source('~/GitHub/PrEP-HIV-Sorting/1 Data cleaning.R')
+
+
+# Compiling all reclassification dfs to a list
+dfs <- vector("list", 100)
+for (i in seq_along(1:100)) {
+        dfs[[i]] <- readRDS(paste("imp", i, ".rds", sep = ""))
+}
+
+### Reclassification analysis ----
+reclass <- vector("list", 100)
+
+for (i in seq_along(1:100)) {
+        
+        dfs[[i]] <- dfs[[i]] %>% filter(ptype == "Main" | ptype == "Casual")
+        
+        # HIV prevalence
+        reclass[[i]]$hiv.imp.n <- table(dfs[[i]]$p_hiv.imp)
+        reclass[[i]]$hiv.imp.p <- prop.table(table(dfs[[i]]$p_hiv.imp))
+
+        # HIV reclassification
+        reclass[[i]]$hiv.reclass.n <- table(dfs[[i]]$p_hiv, dfs[[i]]$p_hiv.imp)
+        reclass[[i]]$hiv.reclass.p <- prop.table(table(dfs[[i]]$p_hiv, dfs[[i]]$p_hiv.imp), 1)
+
+        # HIV-HIV sorting
+        reclass[[i]]$hh.sort.n <- table(dfs[[i]]$hiv2, dfs[[i]]$p_hiv.imp)
+        reclass[[i]]$hh.sort.p <- prop.table(table(dfs[[i]]$hiv2, dfs[[i]]$p_hiv.imp), 1)
+
+        # PrEP prevalence, given neg/unk
+        reclass[[i]]$prep.imp.n <- table(dfs[[i]]$prep.imp)
+        reclass[[i]]$prep.imp.p <- prop.table(table(dfs[[i]]$prep.imp))
+        
+        # PrEP reclassification
+        reclass[[i]]$prep.reclass.n <- table(dfs[[i]]$prep.during.part2, dfs[[i]]$prep.imp)
+        reclass[[i]]$prep.reclass.p <- prop.table(table(dfs[[i]]$prep.during.part2, dfs[[i]]$prep.imp), 1)
+        
+        # HIV prevalence among egos, given partner is HIV -/?
+        reclass[[i]]$ehiv.pneg.n <- table(dfs[[i]]$hiv2[dfs[[i]]$p_hiv.imp == 0])
+        reclass[[i]]$ehiv.pneg.p <- prop.table(table(dfs[[i]]$hiv2[dfs[[i]]$p_hiv.imp == 0]))
+        
+        # HIV-PrEP sorting
+        reclass[[i]]$hp.sort.n <- table(dfs[[i]]$hiv2, dfs[[i]]$prep.imp)
+        reclass[[i]]$hp.sort.p <- prop.table(table(dfs[[i]]$hiv2, dfs[[i]]$prep.imp),1)
+        
+        # PrEP prevalence among egos, given partner is HIV -/?
+        reclass[[i]]$eprep.pneg.n <- table(dfs[[i]]$prep.during.ego2[dfs[[i]]$p_hiv.imp == 0 & dfs[[i]]$hiv2 == 0])
+        reclass[[i]]$eprep.pneg.p <- prop.table(table(dfs[[i]]$prep.during.ego2[dfs[[i]]$p_hiv.imp == 0 & dfs[[i]]$hiv2 == 0]))
+        
+        # PrEP-PrEP sorting
+        reclass[[i]]$pp.sort.n <- table(dfs[[i]]$prep.during.ego2[dfs[[i]]$hiv2 == 0], dfs[[i]]$prep.imp[dfs[[i]]$hiv2 == 0])
+        reclass[[i]]$pp.sort.p <- prop.table(table(dfs[[i]]$prep.during.ego2[dfs[[i]]$hiv2 == 0], dfs[[i]]$prep.imp[dfs[[i]]$hiv2 == 0]), 1)
+        
+        # PrEP-HIV sorting
+        reclass[[i]]$ph.sort.n <- table(dfs[[i]]$prep.during.ego2[dfs[[i]]$hiv2 == 0], dfs[[i]]$p_hiv.imp[dfs[[i]]$hiv2 == 0])
+        reclass[[i]]$ph.sort.p <- prop.table(table(dfs[[i]]$prep.during.ego2[dfs[[i]]$hiv2 == 0], dfs[[i]]$p_hiv.imp[dfs[[i]]$hiv2 == 0]), 1)
+        
+}
+
+listVec <- lapply(reclass, c, recursive=TRUE)
+reclass.results <- as.data.frame(do.call(rbind, listVec))
+
+results <- function(dat, x) {
+        q <- select(dat, starts_with(x))
+        return(t(apply(q, 2, quantile, probs = c(0.025, 0.5, 0.975), na.rm = FALSE)))
+}
+
+# # HIV prevalence
+# results(dat = reclass.results, x = "hiv.imp.n")
+# results(dat = reclass.results, x = "hiv.imp.p")
+# 
+# # HIV reclassification 
+# results(dat = reclass.results, x = "hiv.reclass.n")
+# results(dat = reclass.results, x = "hiv.reclass.p")
+# 
+# # HIV-HIV sorting
+# results(dat = reclass.results, x = "hh.sort.n")
+# results(dat = reclass.results, x = "hh.sort.p")
+# 
+# # PrEP prevalence among HIV -/?
+# results(dat = reclass.results, x = "prep.imp.n")
+# results(dat = reclass.results, x = "prep.imp.p")
+# 
+# # PrEP reclassification
+# results(dat = reclass.results, x = "prep.reclass.n")
+# results(dat = reclass.results, x = "prep.reclass.p")
+# 
+# # HIV prevalence among egos, given partner is HIV -/?
+# results(dat = reclass.results, x = "ehiv.pneg.n")
+# results(dat = reclass.results, x = "ehiv.pneg.p")
+# 
+# # HIV-PrEP sorting
+# results(dat = reclass.results, x = "hp.sort.n")
+# results(dat = reclass.results, x = "hp.sort.p")
+# 
+# # PrEP-PrEP sorting
+# results(dat = reclass.results, x = "pp.sort.n")
+# results(dat = reclass.results, x = "pp.sort.p")
+
+# # PrEP-HIV sorting
+# results(dat = reclass.results, x = "ph.sort.n")
+# results(dat = reclass.results, x = "ph.sort.p")
---
+++ Table4a- HIV-PrEP by ptype.R
@@ -0,0 +1,72 @@
+# Table 4: HIV-PrEP Mixing
+# Required: ~/GitHub/PrEP-HIV-Sorting/Analysis.R
+# By Kevin Maloney (kevin.maloney@emory.edu)
+# 2020-11-19
+
+rm(list = ls())
+
+artnetSort <- artnetSort %>% filter(ptype == "Once")
+
+t4.title <- vector("character", 9)
+t4.title[1] <- "Table 4a. HIV-PrEP Sorting - Persistent Partners"
+
+t4.colname.a <- vector("character", 9) 
+t4.colname.a[4] <- "Partners"
+
+t4.colname.b <- c("", "Total, dyad-level", "", "No PrEP", "", "PrEP", "", "PrEP unknown", "")
+t4.colname.c <- c("Respondents", "N", "Column %", "N", "Row %", "N", "Row %", "N", "Row %")
+
+# Whole sample - partners are HIV -/?
+a <- artnetSort %>% filter(!p_hiv == "Pos") %>% count(hiv3) %>% mutate(perc = round(100*n/sum(n), 1))
+b <- artnetSort %>% filter(!p_hiv == "Pos", hiv3 == "Neg") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+c <- artnetSort %>% filter(!p_hiv == "Pos", hiv3 == "Pos") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+d <- artnetSort %>% filter(!p_hiv == "Pos", hiv3 == "Unk") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+e <- artnetSort %>% filter(!p_hiv == "Pos") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+
+t4.w <- rbind(
+        cbind("Whole Sample", "", "", "", "", "", "", "", ""),
+        cbind("Negative", a$n[1], a$perc[1], b$n[1], b$perc[1], b$n[2], b$perc[2], b$n[3], b$perc[3]),
+        cbind("Positive", a$n[2], a$perc[2], c$n[1], c$perc[1], c$n[2], c$perc[2], c$n[3], c$perc[3]),
+        cbind("Unknown",  a$n[3], a$perc[3], d$n[1], d$perc[1], d$n[2], d$perc[2], d$n[3], d$perc[3]),
+        cbind("Total", sum(a$n), sum(a$perc), e$n[1], e$perc[1], e$n[2], e$perc[2], e$n[3], e$perc[3])
+        )        
+
+# Complete Case - partners are HIV negative and known PrEP use
+a <- artnetSort %>% filter(p_hiv == "Neg", !prep.during.part2 == "Unk", !hiv3 == "Unk") %>% count(hiv3) %>% mutate(perc = round(100*n/sum(n), 1))
+b <- artnetSort %>% filter(p_hiv == "Neg", !prep.during.part2 == "Unk", hiv3 == "Neg") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+c <- artnetSort %>% filter(p_hiv == "Neg", !prep.during.part2 == "Unk", hiv3 == "Pos") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+d <- artnetSort %>% filter(p_hiv == "Neg", !prep.during.part2 == "Unk", !hiv3 == "Unk") %>% count(prep.during.part2) %>% mutate(perc = round(100*n/sum(n), 1))
+
+t4.c <- rbind(
+        cbind("Complete-case", "", "", "", "", "", "", "", ""),
+        cbind("Negative", a$n[1], a$perc[1], b$n[1], b$perc[1], b$n[2], b$perc[2], "", ""),
+        cbind("Positive", a$n[2], a$perc[2], c$n[1], c$perc[1], c$n[2], c$perc[2], "", ""),
+        cbind("Total", sum(a$n), sum(a$perc), d$n[1], d$perc[1], d$n[2], d$perc[2], "", "")
+)
+
+# Reclassification
+a <- results(dat = reclass.results, x = "ehiv.pneg.n")
+b <- results(dat = reclass.results, x = "ehiv.pneg.p")
+c <- as.data.frame(results(dat = reclass.results, x = "hp.sort.n"))
+d <- as.data.frame(results(dat = reclass.results, x = "hp.sort.p"))
+e <- as.data.frame(results(dat = reclass.results, x = "hiv.imp.n"))
+f <- as.data.frame(results(dat = reclass.results, x = "hiv.imp.p"))
+g <- as.data.frame(results(dat = reclass.results, x = "prep.imp.n"))
+h <- as.data.frame(results(dat = reclass.results, x = "prep.imp.p"))
+
+t4.r <- rbind(
+        cbind("Reclassification", "", "", "", "", "", "", "", ""),
+        cbind("Negative/Unknown", "", "", "", "", "", "", "", ""),
+        cbind("Median", round(a[1,2]), round(100*b[1,2],1), round(c[1,2]), round(100*d[1,2],1), round(c[3,2]), round(100*d[3,2],1), "", ""),
+        cbind("95% interval", paste0("(",round(a[1,1]),", ", round(a[1,3]), ")"), paste0("(",round(100*b[1,1],1),", ",round(100*b[1,3],1), ")"), paste0("(",round(c[1,1]),", ", round(c[1,3]), ")"), paste0("(",round(100*d[1,1],1),", ",round(100*d[1,3],1), ")"), paste0("(",round(c[3,1]),", ", round(c[3,3]), ")"), paste0("(",round(100*d[3,1],1),", ",round(100*d[3,3],1), ")"), "", ""),
+        cbind("Positive", "", "", "", "", "", "", "", ""),
+        cbind("Median", round(a[2,2]), round(100*b[2,2],1), round(c[2,2]), round(100*d[2,2],1), round(c[4,2]), round(100*d[4,2],1), "", ""),
+        cbind("95% interval", paste0("(",round(a[2,1]),", ", round(a[2,3]), ")"), paste0("(",round(100*b[2,1],1),", ",round(100*b[2,3],1), ")"), paste0("(",round(c[2,1]),", ", round(c[2,3]), ")"), paste0("(",round(100*d[2,1],1),", ",round(100*d[2,3],1), ")"), paste0("(",round(c[4,1]),", ", round(c[4,3]), ")"), paste0("(",round(100*d[4,1],1),", ",round(100*d[4,3],1), ")"), "", ""),
+        cbind("Total", "", "", "", "", "", "", "", ""),
+        cbind("Median", round(e[1,2]), 100, round(g[1,2]), round(100*h[1,2],1), round(g[2,2]), round(100*h[2,2],1), "", ""),
+        cbind("95% interval", paste0("(",round(e[1,1]),", ", round(e[1,3]), ")"), "(1, 1)", paste0("(",round(g[1,1]),", ", round(g[1,3]), ")"), paste0("(",round(100*h[1,1],1),", ",round(100*h[1,3],1), ")"), paste0("(",round(g[2,1]),", ", round(g[2,3]), ")"), paste0("(",round(100*h[2,1],1),", ",round(100*h[2,3],1), ")"), "", "")        
+)
+
+table4 <- rbind(t4.title, t4.colname.a, t4.colname.b, t4.colname.c, t4.w, t4.c, t4.r)
+
+write.csv(table4, file = "aim1_table4a_HP_pers.csv")
